2d890aa0b002587402e258ee37a2f9e201d84281
Tom McCracken <tomasmccracken@icloud.com>
2024-02-07 15:03:27 +0000

    zoom

--- src/components/linechart.jsx
+++ src/components/linechart.jsx
@@ -3,6 +3,10 @@ import "../App.css";
 
 const LineChart = (props) => {
   const { stockData, newsData, width, height, handleSetArticles } = props;
+  let idleTimeout;
+  const idled = () => {
+    idleTimeout = null;
+  };
   console.log(stockData);
 
   const margin = { top: 20, right: 30, bottom: 30, left: 40 };
@@ -40,12 +44,15 @@ const LineChart = (props) => {
     .range(["#00c7b5", "#7582ff", "#ff71cf"]);
 
   const xAxis = (g) =>
-    g.attr("transform", `translate(0,${height - margin.bottom})`).call(
-      d3
-        .axisBottom(x)
-        .ticks(width / 80)
-        .tickSizeOuter(0),
-    );
+    g
+      .attr("id", "xAxis")
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(
+        d3
+          .axisBottom(x)
+          .ticks(width / 80)
+          .tickSizeOuter(0),
+      );
 
   const yAxis = (g) =>
     g
@@ -93,7 +100,7 @@ const LineChart = (props) => {
       const tooltipText = `${date} ${newsData[article].length} articles`;
       glyphs.push(
         <circle
-          className="stroke-base-200"
+          className="glyphs stroke-base-200"
           key={article}
           cx={x(article)}
           cy={cy}
@@ -110,6 +117,51 @@ const LineChart = (props) => {
     return glyphs;
   };
 
+  const updateChart = (event) => {
+    const extent = event.selection;
+  
+    if (!extent) {
+      if (!idleTimeout) return (idleTimeout = setTimeout(idled, 350));
+      x.domain(d3.extent(stockData, (d) => d.t));
+    } else {
+      x.domain([x.invert(extent[0]), x.invert(extent[1])]);
+      d3.select(".brush").call(brush.move, null);
+    }
+  
+    // Update the line and area paths with the new x-domain
+    d3.select("#line").attr("d", line(stockData));
+    d3.select("#gradient").attr("d", area(stockData));
+  
+    // Update the glyphs with the new x-domain
+    d3.selectAll(".glyphs")
+    .attr("cx", function(d, i) {
+      // Find the corresponding data point in stockData
+      const stock = stockData.find((d) => d.t == d3.select(this).attr("key"));
+  
+      // If the data point exists, return the new cx value
+      if (stock) {
+        return x(stock.t);
+      }
+  
+      // If the data point doesn't exist, return the current cx value
+      return d3.select(this).attr("cx");
+    });
+  
+    // Update the x-axis with the new domain
+    d3.select(".xAxis")
+      .transition()
+      .duration(1000)
+      .call(d3.axisBottom(x).ticks(width / 80));
+  };
+
+  const brush = d3
+    .brushX()
+    .extent([
+      [0, 0],
+      [width, height],
+    ])
+    .on("end", updateChart);
+
   return (
     <div className="linechart">
       <div id="tooltip"></div>
@@ -119,9 +171,13 @@ const LineChart = (props) => {
             <stop offset="0%" stopColor="oklch(var(--p))" stopOpacity={0.8} />
             <stop offset="100%" stopColor="oklch(var(--p))" stopOpacity={0} />
           </linearGradient>
+          <clipPath id="clip">
+            <rect width={width} height={height} x={0} y={0}></rect>
+          </clipPath>
         </defs>
-        <g fill="none">
+        <g id="lineholder" fill="none">
           <path
+            id="line"
             className="stroke-primary"
             strokeWidth="3"
             d={line(stockData)}
@@ -129,6 +185,7 @@ const LineChart = (props) => {
           <path fill="url(#gradient)" d={area(stockData)} />
           <g ref={(node) => d3.select(node).call(xAxis)} />
           <g ref={(node) => d3.select(node).call(yAxis)} />
+          <g ref={(node) => d3.select(node).call(brush)} className="brush"></g>
           {getGlyphs()}
         </g>
       </svg>
